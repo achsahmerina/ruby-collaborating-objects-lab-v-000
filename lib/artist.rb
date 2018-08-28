75428fc478a1d0ed78c5abb87aff742f25b5d14c
Achsah Abraham <achsahmp@gmail.com>
2018-08-28 17:48:27 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Artist
 
   attr_accessor :name, :artist, :songs
@@ -26,6 +27,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
+    binding.pry
       if self.find(name)
         self.find(name)
       else
